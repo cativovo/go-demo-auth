b536ec067905464097fe6ff43121d6d33541031b
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-11-01 19:25:38 +0800

    add register and logout

--- main.go
+++ main.go
@@ -26,7 +26,15 @@ type UserCredentials struct {
 	Password string `json:"password"`
 }
 
-type JsonResponse map[string]interface{}
+type (
+	Json    map[string]interface{}
+	Headers map[string]string
+)
+
+type Response struct {
+	data Json
+	*http.Response
+}
 
 func NewAuthService(project, apiKey string) *AuthService {
 	baseUrl := fmt.Sprintf("https://%s.supabase.co/auth/v1", project)
@@ -36,7 +44,7 @@ func NewAuthService(project, apiKey string) *AuthService {
 	}
 }
 
-func (a *AuthService) fetch(method string, path string, body io.Reader) (JsonResponse, error) {
+func (a *AuthService) fetch(method string, path string, headers Headers, body io.Reader) (*Response, error) {
 	req, err := http.NewRequest(method, a.baseUrl+path, body)
 	if err != nil {
 		return nil, err
@@ -44,6 +52,10 @@ func (a *AuthService) fetch(method string, path string, body io.Reader) (JsonRes
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("apiKey", a.apiKey)
+
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
 
 	res, err := httpClient.Do(req)
 	if err != nil {
@@ -62,26 +74,52 @@ func (a *AuthService) fetch(method string, path string, body io.Reader) (JsonRes
 		r += scanner.Text()
 	}
 
-	j := JsonResponse{}
+	data := Json{}
 
-	if err := json.Unmarshal([]byte(r), &j); err != nil {
+	if err := json.Unmarshal([]byte(r), &data); err != nil {
 		return nil, err
 	}
 
-	return j, nil
+	return &Response{
+			data:     data,
+			Response: res,
+		},
+		nil
 }
 
-func (a *AuthService) Login(email, password string) (JsonResponse, error) {
-	userCredentials := UserCredentials{
-		Email:    email,
-		Password: password,
-	}
+func (a *AuthService) Login(userCredentials UserCredentials) (*Response, error) {
 	payload, err := json.Marshal(userCredentials)
 	if err != nil {
 		return nil, err
 	}
 
-	res, err := a.fetch("POST", "/token?grant_type=password", bytes.NewBuffer(payload))
+	res, err := a.fetch("POST", "/token?grant_type=password", nil, bytes.NewBuffer(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (a *AuthService) Logout(token string) error {
+	headers := Headers{
+		"Authorization": fmt.Sprintf("Bearer %s", token),
+	}
+	_, err := a.fetch("POST", "/logout?scope=local", headers, nil)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
+
+func (a *AuthService) Register(userCredentials UserCredentials) (*Response, error) {
+	payload, err := json.Marshal(userCredentials)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := a.fetch("POST", "/signup", nil, bytes.NewBuffer(payload))
 	if err != nil {
 		return nil, err
 	}
@@ -97,22 +135,70 @@ func main() {
 
 	r.Use(middleware.Logger)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		res, err := authService.Login("test@example.com", "1234")
+	r.Post("/register", func(w http.ResponseWriter, r *http.Request) {
+		userCredentials := UserCredentials{
+			Email:    r.PostFormValue("username"),
+			Password: r.PostFormValue("password"),
+		}
+
+		_, err := authService.Register(userCredentials)
 		if err != nil {
 			log.Println(err)
 			fmt.Fprintln(w, "request error")
+			return
 		}
 
-		j, err := json.Marshal(res)
-		if err != nil {
-			log.Println(err)
-			fmt.Fprintln(w, "json Marshal error")
-		}
+		// if res.StatusCode == http.StatusBadRequest {
+		// 	fmt.Fprintln(w, "Invalid email/password")
+		// 	return
+		// }
 
-		log.Println(res["access_token"])
-		fmt.Fprintln(w, string(j))
+		fmt.Fprintln(w, "registered")
 	})
 
-	http.ListenAndServe("127.0.0.1:3000", r)
+	r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
+		userCredentials := UserCredentials{
+			Email:    r.PostFormValue("username"),
+			Password: r.PostFormValue("password"),
+		}
+
+		res, err := authService.Login(userCredentials)
+		if err != nil {
+			log.Println(err)
+			fmt.Fprintln(w, "request error")
+			return
+		}
+
+		if res.StatusCode == http.StatusBadRequest {
+			fmt.Fprintln(w, "Invalid email/password")
+			return
+		}
+
+		if v, ok := res.data["access_token"].(string); ok {
+			cookie := &http.Cookie{
+				Name:  "jwt",
+				Value: v,
+			}
+
+			http.SetCookie(w, cookie)
+		}
+
+		fmt.Fprintln(w, "logged in")
+	})
+
+	r.Post("/logout", func(w http.ResponseWriter, r *http.Request) {
+		jwt, err := r.Cookie("jwt")
+		if err != nil {
+			return
+		}
+
+		if err := authService.Logout(jwt.Value); err != nil {
+			fmt.Fprintln(w, "ooo")
+			return
+		}
+	})
+
+	if err := http.ListenAndServe("127.0.0.1:3000", r); err != nil {
+		log.Fatal(err)
+	}
 }
